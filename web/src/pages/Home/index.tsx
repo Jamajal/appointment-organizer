77924978794e529de38663c33aed0ac21ebe44d1
Jamajal <leandroliberiomachado@hotmail.com>
2024-04-23 10:39:40 -0300

    Criando setup do frontend

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Página home do site</h1>;
+};
+
+export default Home;
---
+++ web/src/pages/Login/index.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <h1>Página para logar</h1>;
+};
+
+export default Login;
